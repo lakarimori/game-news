d203738c14c14981d1d9e6c50e3cba2590f19c39
Akio <58257252+lakarimori@users.noreply.github.com>
2024-08-21 22:21:17 +0000

    change js to tsx

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-function Home() {
-    return <h1>Oi amor! Estou estudando pra poder ganhar mais dinheiros pra gente ficar rico!</h1>
-}
-
-export default Home;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <p>Oi amor! estou melhorando o código para conseguirmos mais dinheiros!</p>
+        </div>
+    )
+}
+
+export default Home;
