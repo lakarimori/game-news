e140440cbcb358a83588d078c23942f78fbcf19a
Akio <58257252+lakarimori@users.noreply.github.com>
2024-08-30 14:07:47 +0000

    feat: testing mardowns `index.tsx` and color: `#ffffff`

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import MyButton from "../components/button";
 function Home() {
   return (
     <div>
-      <p>Oi amor! estou melhorando o código para conseguirmos mais dinheiros! =D</p>
+      <p>Oi amor! estou melhorando o código para conseguirmos mais dinheiros!</p>
       <MyButton title={"clique no botão! vai mudar o número:"} number={1}></MyButton>
     </div>
   )
